4787457446c751128f48792d40a3e79bcbede2a2
Peng-AP <pzq0406@gmail.com>
2026-02-17 14:31:36 -0500

    exercise 3

--- exercises/3/tests/pittedu.spec.ts
+++ exercises/3/tests/pittedu.spec.ts
@@ -13,26 +13,32 @@ test('TEST-2-LOGO-EXISTS', async ({ page }) => {
 });
 
 test('TEST-3-LOGO-IMAGE', async ({ page }) => {
-  // TODO: Fill in.
-  expect(false).toBeTruthy(); // Placeholder assertion to be replaced with actual test code.
+  await page.goto('https://www.pitt.edu/');
+  await expect(page.getByAltText('University of Pittsburgh', { exact: true })).toHaveAttribute('src', '/sites/default/files/assets/pitt_shield_white-home.png');
 });
 
 test('TEST-4-SCHOOLS-SCI', async ({ page }) => {
-  // TODO: Fill in.
-  expect(false).toBeTruthy(); // Placeholder assertion to be replaced with actual test code.
+  await page.goto('https://www.pitt.edu/');
+  await page.getByRole('button', { name: 'Open Navigation' }).click();
+  await expect(page.getByTestId('block-pitt-25-edu-collegesschools-2').getByRole('listitem').nth(2)).toHaveText('Computing & Information');
 });
 
 test('TEST-5-SCHOOLS-COUNT', async ({ page }) => {
-  // TODO: Fill in.
-  expect(false).toBeTruthy(); // Placeholder assertion to be replaced with actual test code.
+  await page.goto('https://www.pitt.edu/');
+  await page.getByRole('button', { name: 'Open Navigation' }).click();
+  await expect(page.getByTestId('block-pitt-25-edu-collegesschools-2').getByRole('listitem')).toHaveCount(16);
 });
 
 test('TEST-6-SEARCH-CSC', async ({ page }) => {
-  // TODO: Fill in.
-  expect(false).toBeTruthy(); // Placeholder assertion to be replaced with actual test code.
+  await page.goto('https://www.pitt.edu/');
+  await page.getByLabel('Toggle search').click();
+  await page.locator('#edit-keys').fill('computer science club');
+  await page.getByRole('button', { name: 'Search', exact: true }).click();
+  await expect(page.getByRole('link', { name: 'Student Organization Spotlight: Computer Science Club (CSC)' })).toBeVisible();
 });
 
 test('TEST-7-ABOUT-SNAPSHOT', async ({ page }) => {
-  // TODO: Fill in.
-  expect(false).toBeTruthy(); // Placeholder assertion to be replaced with actual test code.
+  await page.goto('https://www.pitt.edu/');
+  await page.getByRole('link', { name: 'About' }).first().click();
+  await expect(page).toHaveScreenshot();
 });
